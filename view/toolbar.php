1d9d7e134f28a228eaf6f24e331cd2a332e9ca84
Jarzon <j@masterj.net>
2018-01-15 16:17:53 -0500

    Make the .primToolbar style more versatile

--- view/toolbar.php
+++ view/toolbar.php
@@ -1,24 +1,17 @@
 <style>
-    .primToolbar .prim, .primToolbar .time, .primToolbar .memory {
-        font-size: 20px;
-        float: left;
-        margin: 5px 5px 0 20px;
-    }
-
-    .primToolbar .time {
-        min-width: 110px;
-    }
-
-    .primToolbar .memory {
-        min-width: 230px;
-    }
-
     .primToolbar {
         position: fixed;
         bottom: 0;
         left: 20px;
         background: #333;
         z-index: 999;
+    }
+
+    .primToolbar div {
+        font-size: 20px;
+        float: left;
+        margin: 5px 5px 0 20px;
+        min-width: 230px;
     }
 </style>
 <div class="primToolbar">
