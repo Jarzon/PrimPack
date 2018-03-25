fbd6b60f9f05711b96b3ab0a53b92343230cdaf2
Jarzon <j@jasonvaillancourt.ca>
2018-03-24 21:43:03 -0400

    Move Prim mail error reporting in PrimPack

--- Controller/Error.php
+++ Controller/Error.php
@@ -9,6 +9,8 @@ use Prim\Controller;
  */
 class Error extends Controller
 {
+    protected $messages = [];
+
     public  $httpErrors = [
         404 => 'Not Found',
         405 => 'Method Not Allowed',
@@ -33,18 +35,54 @@ class Error extends Controller
     /**
      * This method handles the error page that will be shown when a page is not found
      */
-    public function handleError($e, $allowedMethods = '')
+    public function handleError($code, $allowedMethods = '', $e = null)
     {
         $this->cleanOutput();
 
-        header("HTTP/1.1 $e {$this->httpErrors[$e]}");
+        header("HTTP/1.1 $code {$this->httpErrors[$code]}");
 
-        if ($e === 405) {
-            header($allowedMethods);
-            $e = 404;
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = "From: {$this->options['error_mail_from']}";
+
+        $this->addMessage("HTTP code: $code");
+
+        if($e !== null) {
+            $this->addMessage('Type: '.get_class($e));
+            $this->addMessage("Message: {$e->getMessage()}");
+            $this->addMessage("File: {$e->getFile()}");
+            $this->addMessage("Line: {$e->getLine()}");
+
+            // SQL server is down\unreachable
+            if(get_class($e) === 'PDOException') {
+                $code = 503;
+            }
+
+            // The query and params shouldn't be sended by email but logged
+            else if(strpos($e->getMessage(), 'PDO') !== false) {
+                $PDO = $this->container->getPDO();
+
+                $this->addMessage('Query: ' . nl2br($PDO->lastQuery));
+                $this->addMessage('Params: ' . var_export($PDO->lastParams));
+            }
         }
 
-        $this->design("errors/$e", 'PrimPack');
+        $this->addMessage("Uri: {$_SERVER['REQUEST_URI']}");
+
+        $message = wordwrap(implode("\r\n", $this->messages), 70, "\r\n");
+
+        mail($this->options['error_mail'], 'PHP Error', $message, implode("\r\n", $headers));
+
+        if ($code === 405) {
+            header($allowedMethods);
+            $code = 404;
+        }
+
+        $this->design("errors/$code", 'PrimPack');
+    }
+
+    public function addMessage(string $message)
+    {
+        $this->messages[] = $message;
     }
 
     public function debug($e)
