c485ea7fa699d0d42aee943d280f3641cd1f732c
jarzon <j@jasonvaillancourt.ca>
2021-07-22 03:49:41 -0400

    Fix logger config param

--- config/container.php
+++ config/container.php
@@ -3,4 +3,4 @@
 
 $this
     ->register('toolbarService', \PrimPack\Service\Toolbar::class, [$this->get('view'), $this->options['db_enable']? $this->get('pdo'): null, $this->options])
-    ->register('loggerService', \PrimPack\Service\Logger::class, []);
+    ->register('loggerService', \PrimPack\Service\Logger::class, [$this->options]);
