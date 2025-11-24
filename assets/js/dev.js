36a0f91441704ed172d49c7a9ef1b1ac0f512685
Jarzon <jason@libellum.ca>
2025-11-24 08:08:36 -0500

    Delay dev.js execution after the html is loaded

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -1,38 +1,41 @@
-const urls = [
-    window.location.href,
-    ...Array.from(document.querySelectorAll('link[rel="stylesheet"]')).map(l => l.href),
-    ...Array.from(document.querySelectorAll('script[src]')).map(s => s.src)
-];
+document.addEventListener('DOMContentLoaded', function() {
+    const urls = [
+        window.location.href,
+        ...Array.from(document.querySelectorAll('link[rel="stylesheet"]')).map(l => l.href),
+        ...Array.from(document.querySelectorAll('script[src]')).map(s => s.src)
+    ];
 
-const lastModified = {};
-let currentIndex = 0;
+    const lastModified = {};
+    let currentIndex = 0;
 
-async function checkUpdates() {
-    const url = urls[currentIndex];
+    async function checkUpdates() {
+        if(currentIndex === -1) return;
+        const url = urls[currentIndex];
 
-    try {
-        const res = await fetch(url, { method: 'HEAD', cache: 'no-store' });
-        const mod = res.headers.get('last-modified');
-        if (!mod || res.status !== 200) return;
+        try {
+            const res = await fetch(url, { method: 'HEAD', cache: 'no-store' });
+            const mod = res.headers.get('last-modified');
+            if (!mod || res.status !== 200) return;
 
-        if (lastModified[url] && lastModified[url] !== mod) {
-            console.log(`[dev-reload] Change detected in: ${url}`);
+            if (lastModified[url] && lastModified[url] !== mod) {
+                console.log(`[dev-reload] Change detected in: ${url}`);
+                currentIndex = -1;
 
-            // make sure PHP config opcache.revalidate_freq is at 0
-            location.reload();
-            return;
+                window.location.reload();
+                return;
+            }
+
+            lastModified[url] = mod;
+        } catch (e) {
+            console.warn(`[dev-reload] Failed to check ${url}:`, e);
         }
 
-        lastModified[url] = mod;
-    } catch (e) {
-        console.warn(`[dev-reload] Failed to check ${url}:`, e);
+        if(currentIndex >= urls.length - 1) {
+            currentIndex = 0;
+        } else {
+            currentIndex++;
+        }
     }
 
-    if(currentIndex >= urls.length - 1) {
-        currentIndex = 0;
-    } else {
-        currentIndex++;
-    }
-}
-
-setInterval(checkUpdates, 150);
+    setInterval(checkUpdates, 150);
+})
