81d9588c205bf177d53721e8274e04c8211fbebf
Jarzon <jason@libellum.ca>
2025-12-24 22:44:38 -0500

    Add an action in prim toolbar to be able to pause/unpause dev auto reload

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let pause = false;
+    let toolbar = document.querySelector('.primToolbar #tools');
+
+    function setColorState(div) {
+        div.style.color = !pause? '#1a9500' : '#770000';
+    }
+
+    if(toolbar) {
+        let div = document.createElement('div');
+        div.innerText = 'Dev.js';
+        setColorState(div);
+        div.addEventListener('click', () => {
+            pause = !pause;
+            setColorState(div);
+        });
+        toolbar.appendChild(div);
+    }
+
     const urls = [
         window.location.href,
         ...Array.from(document.querySelectorAll('link[rel="stylesheet"]')).map(l => l.href),
@@ -8,43 +26,45 @@ document.addEventListener('DOMContentLoaded', function() {
     const lastModified = {};
 
     async function checkUpdates(currentIndex, skipUntil) {
-        if(currentIndex === -1) return;
-        const url = urls[currentIndex];
+        if(!pause) {
+            if(currentIndex === -1) return;
+            const url = urls[currentIndex];
 
-        try {
-            const res = await fetch(url, { method: 'HEAD', cache: 'no-store' });
-            const mod = res.headers.get('last-modified');
+            try {
+                const res = await fetch(url, { method: 'HEAD', cache: 'no-store' });
+                const mod = res.headers.get('last-modified');
 
-            if (res.status !== 200) {
-                skipUntil = currentIndex;
-                throw new Error(`${url} returned status code ${res.status}`);
-            }
-            if(skipUntil !== null) {
-                if(currentIndex !== skipUntil) {
-                    throw new Error(`Skipping until current index(${currentIndex}) go back to (${skipUntil})`);
+                if (res.status !== 200) {
+                    skipUntil = currentIndex;
+                    throw new Error(`${url} returned status code ${res.status}`);
                 }
-                skipUntil = null;
+                if(skipUntil !== null) {
+                    if(currentIndex !== skipUntil) {
+                        throw new Error(`Skipping until current index(${currentIndex}) go back to (${skipUntil})`);
+                    }
+                    skipUntil = null;
+                }
+
+                if (!mod) throw new Error(`No last-modified was found for ${url}`);
+
+                if (lastModified[url] && lastModified[url] !== mod) {
+                    console.log(`[dev-reload] Change detected in: ${url}`);
+                    currentIndex = -1;
+
+                    window.location.reload();
+                    return;
+                }
+
+                lastModified[url] = mod;
+            } catch (e) {
+                console.warn(`[dev-reload] ${e.message}`);
             }
 
-            if (!mod) throw new Error(`No last-modified was found for ${url}`);
-
-            if (lastModified[url] && lastModified[url] !== mod) {
-                console.log(`[dev-reload] Change detected in: ${url}`);
-                currentIndex = -1;
-
-                window.location.reload();
-                return;
+            if(currentIndex >= urls.length - 1) {
+                currentIndex = 0;
+            } else {
+                currentIndex++;
             }
-
-            lastModified[url] = mod;
-        } catch (e) {
-            console.warn(`[dev-reload] ${e.message}`);
-        }
-
-        if(currentIndex >= urls.length - 1) {
-            currentIndex = 0;
-        } else {
-            currentIndex++;
         }
 
         setTimeout(() => {
